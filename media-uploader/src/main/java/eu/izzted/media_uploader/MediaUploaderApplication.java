cc651f5a114009d838048a9d00d6e22220feed09
Martin Agersted Jarl <martinjarl@icloud.com>
2025-06-10 14:58:50 +0200

    Adding media uploader. Adjusting gitignore

---
+++ media-uploader/src/main/java/eu/izzted/media_uploader/MediaUploaderApplication.java
@@ -0,0 +1,13 @@
+package eu.izzted.media_uploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaUploaderApplication.class, args);
+	}
+
+}
---
+++ media-uploader/src/test/java/eu/izzted/media_uploader/MediaUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.izzted.media_uploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
