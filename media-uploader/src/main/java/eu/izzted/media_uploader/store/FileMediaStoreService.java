b580be748f092c13100fbecfd4036f9ac378f929
Martin Agersted Jarl <martinjarl@icloud.com>
2025-06-12 15:15:42 +0200

    Can upload

--- media-uploader/src/main/java/eu/izzted/media_uploader/store/FileMediaStoreService.java
+++ media-uploader/src/main/java/eu/izzted/media_uploader/store/FileMediaStoreService.java
@@ -6,6 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Service
 public class FileMediaStoreService implements MediaStoreService {
 
@@ -19,13 +25,46 @@ public class FileMediaStoreService implements MediaStoreService {
         this.mediaStore = config.getLocation();
     }
 
+
     @Override
     public void storeFile(MultipartFile file) {
         log.info("Will save file {} to store... at {}",
                 file.getOriginalFilename(),
                 mediaStore);
 
+        Path mPath = Paths.get(this.mediaStore);
+
+        if (!this.validateConfig()) {
+            log.error("Path {} does not exist. {}", mPath, mPath.toAbsolutePath());
+            return;
+        }
+
+        String filename = file.getOriginalFilename();
+        if (filename == null) {
+            log.error("No filename");
+            return;
+        }
+
+        Path destFile = mPath.resolve(filename);
+        try {
+            Files.copy(file.getInputStream(), destFile);
+        } catch (IOException e) {
+            log.error("Could not copy file: {}", e.getLocalizedMessage());
+            return;
+        }
+
         log.info("... Has saved {}", file.getOriginalFilename());
     }
+
+
+    public boolean validateConfig() {
+        if (mediaStore.trim().isEmpty()) {
+            return false;
+        }
+
+        Path mediaStorePath = Paths.get(this.mediaStore);
+        return Files.exists(mediaStorePath.toAbsolutePath());
+    }
+
 
 }
--- media-uploader/src/main/java/eu/izzted/media_uploader/store/MediaStoreService.java
+++ media-uploader/src/main/java/eu/izzted/media_uploader/store/MediaStoreService.java
@@ -4,4 +4,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface MediaStoreService {
     void storeFile(MultipartFile file);
+    boolean validateConfig();
 }
