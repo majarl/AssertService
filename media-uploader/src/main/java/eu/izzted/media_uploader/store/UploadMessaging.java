d21dcf66b46bc95e63313218c9879aaa062920dc
Martin Agersted Jarl <martinjarl@icloud.com>
2025-06-14 18:29:17 +0200

    Changing config method

--- media-uploader/src/main/java/eu/izzted/media_uploader/store/UploadMessaging.java
+++ media-uploader/src/main/java/eu/izzted/media_uploader/store/UploadMessaging.java
@@ -1,6 +1,8 @@
 package eu.izzted.media_uploader.store;
 
 import eu.izzted.media_uploader.ResultOf;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,8 @@ import java.nio.file.Path;
 @Component
 public class UploadMessaging {
 
+    private static final Logger log = LoggerFactory.getLogger(UploadMessaging.class);
+
     private final RabbitTemplate template;
 
     private final Queue queue;
@@ -18,10 +22,13 @@ public class UploadMessaging {
     public UploadMessaging(RabbitTemplate rabbitTemplate, String queueName) {
         this.template = rabbitTemplate;
         this.queue = new Queue(queueName);
+        log.info("Has created UploadMessaging with: {} and queueName {}",
+                this.template, this.queue);
     }
 
     public ResultOf<String> fileNotice(Path file) {
         String filename = String.valueOf(file.getFileName());
+        log.info("Will send notice about {} to {}", filename, queue.getName());
         this.template.convertAndSend(queue.getName(), filename);
         return ResultOf.success("Has send: " + filename);
     }
--- media-uploader/src/main/java/eu/izzted/media_uploader/store/UploadMessagingConfig.java
+++ media-uploader/src/main/java/eu/izzted/media_uploader/store/UploadMessagingConfig.java
@@ -1,16 +1,19 @@
 package eu.izzted.media_uploader.store;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class UploadMessagingConfig {
 
-    private String queueName = "fileNotice";
+    @Value("${file.queue.name}")
+    private String queueName;
 
     @Bean
     public UploadMessaging uploadMessaging(RabbitTemplate rabbitTemplate) {
+        System.out.println(" -----> " + queueName);
         return new UploadMessaging(rabbitTemplate, queueName);
     }
 
