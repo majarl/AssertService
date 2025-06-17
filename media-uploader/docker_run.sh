ffec7cca84145c41a458ccea2bd8c89fd484e68a
Martin Agersted Jarl <martinjarl@icloud.com>
2025-06-17 12:10:19 +0200

    Binding volume on host system

--- media-uploader/docker_run.sh
+++ media-uploader/docker_run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -d --name media-uploader -p 8080:8080 media-uploader
+docker run --mount type=bind,src=./upload_dir,dst=/mnt/media_upload_dir \
+  -d --name media-uploader \
+  -p 8080:8080 media-uploader
