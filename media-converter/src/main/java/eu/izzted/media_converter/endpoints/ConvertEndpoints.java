01e564177675d97fab6a9b508521a4790f29f067
Martin Agersted Jarl <martinjarl@icloud.com>
2025-06-19 13:40:55 +0200

    Basic experiment with executing system calls

--- media-converter/src/main/java/eu/izzted/media_converter/endpoints/ConvertEndpoints.java
+++ media-converter/src/main/java/eu/izzted/media_converter/endpoints/ConvertEndpoints.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 @RestController
 @RequestMapping("/api/converter")
 public class ConvertEndpoints {
@@ -13,6 +16,32 @@ public class ConvertEndpoints {
         return "Convert endpoints";
     }
 
+    @GetMapping("/pwd")
+    public String pwd() {
+        String[] commands = { "ffmpeg", "-version" };
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command(commands);
 
+        try {
+            Process p = processBuilder.start();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(p.getInputStream()));
+
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+            int exitCode = p.waitFor();
+
+            output.append("Exit code: ")
+                    .append(exitCode);
+
+            return output.toString();
+        } catch (Exception e) {
+            return e.getLocalizedMessage();
+        }
+    }
 
 }
