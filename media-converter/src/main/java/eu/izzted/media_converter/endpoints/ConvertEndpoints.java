23f9c1fc6d8dec0efe9952377fcf030bc30fc5ab
Martin Agersted Jarl <martinjarl@icloud.com>
2025-06-20 14:40:10 +0200

    Testing ffmpeg convert

--- media-converter/src/main/java/eu/izzted/media_converter/endpoints/ConvertEndpoints.java
+++ media-converter/src/main/java/eu/izzted/media_converter/endpoints/ConvertEndpoints.java
@@ -1,11 +1,14 @@
 package eu.izzted.media_converter.endpoints;
 
+import org.apache.tomcat.jni.Buffer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api/converter")
@@ -18,7 +21,7 @@ public class ConvertEndpoints {
 
     @GetMapping("/pwd")
     public String pwd() {
-        String[] commands = { "ffmpeg", "-version" };
+        String[] commands = { "pwd" };
         ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.command(commands);
 
@@ -42,6 +45,44 @@ public class ConvertEndpoints {
         } catch (Exception e) {
             return e.getLocalizedMessage();
         }
+    }
+
+    @GetMapping("/convert_test")
+    public String convertTest() {
+        // ffmpeg -i oregano.mov -codec:v h264 -codec:a aac -map 0 -f hls -hls_time 10 -hls_list_size 0 output.m3u8
+        String cmd = "ffmpeg -i oregano.mov -codec:v h264 -codec:a aac -map 0 -f hls -hls_time 10 -hls_list_size 0 output.m3u8";
+
+        String[] commands = cmd.split(" ");
+
+        System.out.println("---> commands: \n" + Arrays.toString(commands));
+
+
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.command(commands);
+
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println("---> STARTING...");
+        try {
+            Process p = pb.start();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(p.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+
+            int exitCode = p.waitFor();
+            System.out.println("---> Ended with code: " + exitCode);
+
+            sb.append("    Exit code: ")
+                    .append(exitCode);
+        } catch (IOException | InterruptedException e) {
+            sb.append(e.getLocalizedMessage());
+        }
+
+        return sb.toString();
     }
 
 }
